e29ccfaf96fa626597967c99fcb5dca33823c6f4
AlfredR <alfred.rundqvist@gmail.com>
2021-11-02 17:15:26 +0100

    Added router in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      <div>
-      </div>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/:code/:title">
+            <p>Litteraturinfosida</p>
+            {/*
+              LITTERATURINFO
+                HEADER
+                SÖKVÄG
+                LITTERATUR
+            */}
+        </Route>
+        <Route path="/:code">
+            <p>Kursinfosida</p>
+            {/* 
+              KURSINFOSIDAN
+                HEADER
+                SÖKVÄG
+                LITTERATURMODULER
+                KURSMODULER
+                ...
+            */}
+        </Route>
+        <Route path="/">
+          <p>Söksida</p>
+          {/* 
+            LOGGA
+            SÖKRUTA
+            KURSMODULER (RESULTAT)
+          */}
+          
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
