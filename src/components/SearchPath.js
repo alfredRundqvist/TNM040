2aea13cded6d40844cc2b69288f20056287dd90c
AlfredR <alfred.rundqvist@gmail.com>
2021-11-03 18:21:08 +0100

    Added simple searchpath

---
+++ src/components/SearchPath.js
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+const SearchPath = ({props}) => {
+    // Hämtar den aktuella sökvägen och delar upp den i en array vid varje '/'
+    const path = window.location.pathname;
+    const pathComponents = path.split("/");
+    
+    const linkStyle = {
+        textDecoration: "none",
+        color: "#4663AC"
+    }
+
+    return (
+        <div>
+            {pathComponents.map((d,i) => {
+                if (i == 0){
+                    return  <Link key={i} to="/" style={linkStyle}>Sök/</Link>;
+                }
+                else if (i == 1){
+                    // ../ för att sökvägen ska länka rätt
+                    
+                    return <Link key={i} to={d} style={linkStyle}>{d}/</Link>
+                }
+                // TODO: Lägg till länk om i == 2, dvs litteraturinfosidan
+                // TODO: Eventuellt, ändra kurskod till kursnamn
+            })}
+        </div>
+    )
+};
+
+export default SearchPath;
