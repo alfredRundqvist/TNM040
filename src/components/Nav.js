259de8227848b670e1d5ae952eb62470efcfb425
AlfredR <alfred.rundqvist@gmail.com>
2021-11-05 17:15:33 +0100

    Added img to nav comp

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,7 @@ import {
   Link
 } from "react-router-dom"; 
 import '../css/Nav.css';
+import backArrow from '../images/tillbakapil.png';
 // Kommentar: Bättre med titel som props istället för kursnamn? Eftersom nav används för både litteraturinfo och kursinfo.
 function Nav(kursnamn) {
 
@@ -10,7 +11,7 @@ console.log(kursnamn);
   return (
     <div>
       <div> 
-        <Link to={"../"}> <div> &lt; </div></Link> {/* TODO: Hitta "ett steg bakåt"istället för homepage*/}
+        <Link to={"../"}> <img src={backArrow} alt="Back" /></Link> {/* TODO: Hitta "ett steg bakåt"istället för homepage*/}
       </div>
       <div>
         <p>{kursnamn.header}</p>
