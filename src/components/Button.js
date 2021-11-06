505c11b55f8fee10bbfa7d82afbefdc2ed86f4c5
AlfredR <alfred.rundqvist@gmail.com>
2021-11-06 19:37:46 +0100

    Updated CSS

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,20 +6,31 @@ import toUrl from './toUrl'
   const Button = ({objekt}) => {
   const divStyle = {
     height: "10vh", 
-    backgroundColor: "blue"
+    backgroundColor: "#DCF0FF",
+    textAlign: "center",
+    marginTop: "4%",
+    width: "95%",
+    borderRadius: "10px",
+    boxShadow: "5px 5px 5px #4663AC33"
   }
 
   const LinkStyle = {
-    backgroundColor: "green",
-    height: "2vh"
+    color: "#4663AC",
+    textDecoration: "none"
+    
   }
-console.log(objekt.name + ":)))")
 
   return (
+    // Jag tänker att kurskod vore bättre att ha som URL. Kortare och slipper konvertera. /Alfred
+    <Link to= {"./" + toUrl(objekt.namn)} style={LinkStyle}>
     <div style = {divStyle}>
- <Link to= {"./" + toUrl(objekt.namn)} style = {LinkStyle} >HEJ</Link>
- <p>Namn:{objekt.namn}</p>
+      <span>{objekt.namn}</span>
+      <br />
+      <br/>
+      <span>{objekt.kurskod}</span>
+
     </div>
+    </Link>
   );
 }
 export default Button;
