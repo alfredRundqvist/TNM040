292a547be66bbec46f4364d912559718d5a17001
AlfredR <alfred.rundqvist@gmail.com>
2021-11-10 08:48:49 +0100

    Button edit

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,35 +5,54 @@ import toUrl from './toUrl'
 
   const Button = ({rubrik, underrubrik, url}) => {
   const divStyle = {
-    height: "10vh", 
+    height: "15vh", 
     backgroundColor: "#DCF0FF",
     textAlign: "center",
     marginTop: "4%",
     width: "95%",
     borderRadius: "10px",
-    boxShadow: "5px 5px 5px #4663AC33",
+    boxShadow: "3px 3px 5px #0008",
     display: "flex",
-    justifyContent: "center",
+    flexDirection: "row",
     alignItems: "center",
-    flexDirection: "column"
+    fontSize: "3vh"
   }
 
   const LinkStyle = {
     color: "#4663AC",
-    textDecoration: "none"
+    textDecoration: "none",
     
   }
-  
+  const arrowStyle={
+    fontSize: "6vh"
+  }
 
+  const textBox = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    width: "90%"
+  }
+
+  const underRubrikStyle = {
+    // Om vi vill ändra position för kurskoden
+
+  }
   return (
 
     <Link to= {"./" + url} style={LinkStyle}>
-    <div style = {divStyle}>
-      <span>{rubrik}</span>
-      
-      {underrubrik !== undefined? <span>{underrubrik}</span> : null}
-      
+    <div style={divStyle}>
+      <div style={textBox}>
+        <span>{rubrik}</span>
+        
+        {underrubrik !== undefined? <span style={underRubrikStyle}>{underrubrik}</span> : null}
+        
 
+      </div>
+      <div style={arrowStyle}>
+        &gt;
+      </div>
     </div>
     </Link>
   );
